76e63e188a102bf70092ea8b6603e0fce09a7282
w2xi <43wangxi@gmail.com>
2021-03-03 21:39:04 +0800

    init project && install basic package as development preparation upfront

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>Home</div>
+</template>
+
+<script>
+export default {
+	name: 'Home',
+	data(){
+		return {
+
+		}
+	},
+	computed: {},
+	methods: {},
+	created(){},
+	mounted(){},
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
