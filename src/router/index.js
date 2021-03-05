30f53d2fb24dfe1ad5a7ba0fc3c1d199b8284fb2
w2xi <43wangxi@gmail.com>
2021-03-05 13:05:19 +0800

    resolve conflict

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/pages/home/Home'
-<<<<<<< HEAD
-import MyPage  from '@/pages/my/MyPage'
-=======
 import MyPage from '@/pages/my/My'
 import Login from '@/components/Login'
 import Miss from '@/components/Miss'
->>>>>>> 048b05c7c89189d817477a0fa393df4a730d4361
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
-<<<<<<< HEAD
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/my',
-      name: 'My',
-      component: MyPage
-    },
-  ]
-=======
 	routes: [
 		{
 			path: '/home',
@@ -49,5 +31,4 @@ export default new VueRouter({
 			component: Miss,
 		},
 	]
->>>>>>> 048b05c7c89189d817477a0fa393df4a730d4361
 })
