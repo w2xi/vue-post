824c6bd7bd1f322dc7b448f8f1201d3b9d048c59
w2xi <43wangxi@gmail.com>
2021-03-05 12:45:46 +0800

    add route

---
+++ src/pages/my/MyPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>my page</div>
+</template>
+
+<script>
+export default {
+  name: 'MyPage',
+
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/pages/home/Home'
+import MyPage  from '@/pages/my/MyPage'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
-	routes: [
-		{
-			path: '/',
-			name: 'Home',
-			component: Home
-		},
-	]
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/my',
+      name: 'My',
+      component: MyPage
+    },
+  ]
 })
