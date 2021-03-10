bdd35f8dc9d4005c1a7ee871373c975222486eb3
w2xi <43wangxi@gmail.com>
2021-03-10 14:36:51 +0800

    add blog-panel component

--- src/pages/blog/Blog.vue
+++ src/pages/blog/Blog.vue
@@ -13,7 +13,14 @@
     </div>
     <!-- blog list -->
     <div class="blog-list">
-      <blog-card />
+      <div class="blog-item">
+        <blog-card />
+        <blog-panel />
+      </div>
+      <div class="blog-item">
+        <blog-card />
+        <blog-panel />
+      </div>
     </div>
   </div>
 </template>
@@ -21,9 +28,11 @@
 <script>
 import SearchBar from '@/components/Search'
 import BlogCard from './components/BlogCard'
+import BlogPanel from './components/BlogPanel'
 
 export default {
   components: {
+    BlogPanel,
     BlogCard,
     SearchBar,
   },
@@ -35,13 +44,19 @@ export default {
 
 .container
   height: 100vh
-  width: 100vw
+  overflow-y: auto
   background: $backgroundGrey
 .header
+  position: fixed
+  top: 0
+  width: 100%
+  height: 50px
   display: flex
   align-items: center
-  padding: 7px
+  box-sizing: border-box
+  padding: 0 8px
   background: #fff
+  z-index: 10
   .blog-post
     width: 35px
     text-align: center
@@ -49,4 +64,8 @@ export default {
       font-size: 21px
   .blog-search
     flex: 1  
+.blog-list
+  margin-top: 56px
+  .blog-item + .blog-item
+    margin-top: 7px
 </style>
---
+++ src/pages/blog/components/BlogPanel.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="blog-panel">
+    <ul class="panel-box">
+      <li class="panel-item">
+        <i class="iconfont icon-share"></i>
+        <span class="title">分享</span>
+      </li>
+      <li class="panel-item">
+        <i class="iconfont icon-comment1"></i>
+        <span class="title">1212</span>
+      </li>
+      <li class="panel-item">
+        <i class="iconfont icon-good"></i>
+        <span class="title">1212</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/variables.styl'
+
+.panel-box
+  display: flex
+  align-items: center
+  background: #fff
+  .panel-item
+    display: flex
+    align-items: center
+    padding: 10px 18px
+    .iconfont
+      font-size: 20px
+      margin-right: 3px
+      color: #999
+    .title
+      color: #999
+</style>
