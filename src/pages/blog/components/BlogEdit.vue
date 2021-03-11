59a3cd0f4e4c3b8486be522743402c1ccda99883
w2xi <43wangxi@gmail.com>
2021-03-11 18:05:35 +0800

    modify blog-edit component

--- src/pages/blog/components/BlogEdit.vue
+++ src/pages/blog/components/BlogEdit.vue
@@ -12,12 +12,20 @@
 		</textarea>
 		<div class="notice">最多发布九张图片</div>
 		<div class="images-wrapper">
-			<div class="image-item-box">
-				<img class="image" src="@/assets/images/default-avatar.jpg" />
+			<div 
+				v-show="imgSrcList.length"
+				class="image-item-box" 
+				v-for="(item,index) of imgSrcList"
+				:key="index"
+			>
+				<div class="icon-delete-box">
+					<i class="iconfont icon-shanchu"></i>
+				</div>
+				<img class="image" :src="item" />
 			</div>
 			<div class="add-image-padding">
 				<div class="add-image" @click="$refs.file.click()">
-					<i class="iconfont icon-tianjia"></i>
+						<i class="iconfont icon-tianjia"></i>
 					<input ref="file" type="file" @change="uploadHandle" multiple hidden>
 				</div>
 			</div>
@@ -35,8 +43,8 @@ export default {
 	data(){
 		return {
 			content: '',
-			imageList: [],
-			// 允许上传的图片类型
+			imgSrcList: [],
+			imgFileList: [],
 			allowImgType: [
 				'image/png', 'image/jpg', 'image/jpeg', 'image/gif',
 			],
@@ -49,28 +57,34 @@ export default {
 	methods: {
 		uploadHandle(e){
 			const files = e.target.files
-			if ( files.length > this.maxImgCounts ){
+			if ( files.length > this.maxImgCounts || this.imgFileList.length > this.maxImgCounts ){
 				return Toast('最多只能上传9张图片')
 			}
+			
 			for ( const file of files ){
 				if ( !this._validType(file.type) ){
 					return Toast('图片类型错误')
 				}
 				if ( !this._validMaxSize(file.size) ){
 					return Toast('图片大小不能超过1M')
-				} 
+				}
+				const reader = new FileReader()
+				// Starts reading the contents of the specified Blob, once finished, the result attribute contains a data: URL representing the file's data.
+				reader.readAsDataURL(file)
+				// A handler for the load event. This event is triggered each time the reading operation is successfully completed.
+				reader.onload = ()=>{
+					this.imgSrcList.push(reader.result)
+				}
+				this.imgFileList.push(file)
 			}
-			// convert files to a array-like
-			Array.from(files).map((file)=>{
-				this.imageList.push(file)
-			})
-			// solving can't upload same image
+			// solving can't upload the same image
 			e.target.value = null
-			console.log(this.imageList)
 		},
+		// validate image type
 		_validType(type){
 			return this.allowImgType.indexOf(type) !== -1
 		},
+		// validate image size
 		_validMaxSize(size){
 			return size <= this.maxImgSize
 		}
@@ -92,10 +106,20 @@ export default {
 	padding: 10px
 	font-size: 15px
 	color: #666
-
 .images-wrapper
 	display: flex
 	flex-wrap: wrap
+.image-item-box
+	position: relative
+	.icon-delete-box
+		position: absolute
+		top: 5px
+		right: 5px
+	.icon-delete-box .icon-shanchu
+		position: absolute
+		top: 5px
+		right: 5px
+		color: #fff
 .image-item-box, .add-image-padding
 	height: 100px
 	width: 100px
