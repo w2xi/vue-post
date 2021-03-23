e717c679b121250660560d3c240af93a88046713
w2xi <43wangxi@gmail.com>
2021-03-23 23:39:39 +0800

    blog-detail style

--- src/pages/blog/components/BlogDetail.vue
+++ src/pages/blog/components/BlogDetail.vue
@@ -3,6 +3,23 @@
 		<div class="blog-detail">
 			<blog-card :blog="blog" />
 		</div>
+		<div class="comment-wrapper">
+			<div class="header">
+				<div class="title">
+					评论区
+					<span class="comment-count">12</span>
+				</div>
+				<div class="order-category">
+					<ul>
+						<li class="time">时间</li>
+						<li class="hot">热门</li>
+					</ul>
+				</div>
+			</div>
+			<div class="comment-list">
+				
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -18,13 +35,8 @@ export default {
 			blog: null,
 		}
 	},
-	watch: {
-		// $route(to, from){
-		// 	const blogId = this.$route.params.id
-		// 	if ( blogId ){
-		// 		this.getBlogDetail(blogId)
-		// 	}
-		// }
+	created(){
+		this.getBlogDetail(this.$route.params.id)
 	},
 	methods: {
 		async getBlogDetail(id){
@@ -38,15 +50,38 @@ export default {
 			}
 		}
 	},
-	created(){
-		const blogId = this.$route.params.id
-		if ( blogId ){
-			this.getBlogDetail(blogId)
-		}
-	},
 }
 </script>
 
 <style lang="stylus" scoped>
+@import '~@/assets/styles/variables.styl' 
 
+.container 
+	height: 100vh
+	background: $backgroundGrey
+.comment-wrapper
+	margin-top: 10px
+	background: #fff
+	.header
+		display: flex
+		padding: 20px 10px
+	.header .title
+		flex: 1
+		font-weight: 600
+		font-size: 15px
+		.comment-count
+			font-weight: normal
+			color: #777
+			letter-spacing: .8px
+	.header .order-category
+		width: 150px
+		font-size: 14px
+		ul 
+			display: flex
+			flex-direction: row-reverse
+		ul li
+			padding: 0 15px
+			color: #444
+		li + li 
+			border-right: 1px solid #ccc
 </style>
